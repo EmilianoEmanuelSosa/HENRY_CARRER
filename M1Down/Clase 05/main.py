4826a1f3d65328700987f1b7e62ae734207c012f
EmilianoEmanuelSosa <emilianoemanuel2850@gmail.com>
2023-04-15 17:01:53 -0300

    Game of stack

--- M1Down/Clase 05/main.py
+++ M1Down/Clase 05/main.py
@@ -13,3 +13,51 @@ import random
 
 
 class game():
+    def __init__(self):
+        pass
+
+    def playGame(self):
+        print('\n\n\nThe object of the game is to draw numbers from a list without the total sum exceeding 50. \nIf you stop and the next sum exceeds 50, you win with 10 points. If the sum is greater than 50,\n you lose. If the sum is less than 50, we subtract one point for each number you could have rolled without exceeding 50.')
+        print("\nLET'US BEGIN!\n")
+        lis = [random.randint(1, 21) for i in range(20)]
+        sum = lis.pop()
+        print(f'\nWas removed the: {sum}')
+        while True:
+            asw = str(
+                input('\nyou want to continue?, and remove a number more? '))
+            if not (asw.lower() in ('yes', 'no')):
+                print('\nIntroduce yes or no')
+            elif asw.lower() == 'yes':
+                num_aprox = lis.pop()
+                sum += num_aprox
+                print(f'\nWas removed the:{num_aprox}')
+            elif asw.lower() == 'no':
+                if (sum < 50) & (sum+lis.pop() > 50):
+                    print(
+                        f"\nWOW YOU WIN, YOU'RE AMAZING! you win With: {sum}")
+                    break
+                elif (sum < 50) & (sum+lis.pop() < 50):
+                    count = 0
+                    while sum < 50:
+                        count += 1
+                        sum += lis.pop()
+                    print(f'\nYou win, with to {count}')
+                    break
+                if sum > 50:
+                    print('\nYOU LOSE,sorry try again')
+                    break
+        return None
+
+
+games = game()
+games.playGame()
+
+
+# * 2) Implementar un juego donde constas de 2 jarras, de capacidad 5 y 3 litros respectivamente, y debes colocar 4 litros en la jarra de 5L.
+# Las opciones posibles son:
+#  Llenar la jarra de 3 litros
+#  Llenar la jarra de 5 litros
+#  Vaciar la jarra de 3 litros
+#  Vaciar la jarra de 5 litros
+#  Verter el contenido de la jarra de 3 litros en la de 5 litros
+#  Verter el contenido de la jarra de 5 litros en la de 3 litros
