99586dd48112bc9ac6fd408c4e4dea1c981beb48
EmilianoEmanuelSosa <emilianoemanuel2850@gmail.com>
2023-04-17 13:59:22 -0300

    Advange of problem of jugs

--- M1Down/Clase 05/main.py
+++ M1Down/Clase 05/main.py
@@ -64,17 +64,14 @@ class game():
                         sum += lis.pop()
                     print(f'\nYou win, with to {count}')
                     break
-                if sum > 50:
+                elif sum > 50:
                     print('\nYOU LOSE,sorry try again')
                     break
         return None
 
 
-games = game()
-games.playGame()
-
-
-# * 2) Implementar un juego donde constas de 2 jarras, de capacidad 5 y 3 litros respectivamente, y debes colocar 4 litros en la jarra de 5L.
+# * 2) Implementar un juego donde constas de 2 jarras, de capacidad 5 y 3
+# litros respectivamente, y debes colocar 4 litros en la jarra de 5L.
 # Las opciones posibles son:
 #  Llenar la jarra de 3 litros
 #  Llenar la jarra de 5 litros
@@ -90,9 +87,113 @@ class jug3():
 
     def full(self):
         self.jug = 3
+        return self.jug
+
+    def value(self):
+        return self.jug
+
+    def empty(self):
+        self.jug = 0
+        return 0
+
+    def receiver(self, value):
+        if value < 3:
+            value -= 3
+            self.jug = value
+            return value
+
+    def transfersend(self):
+        if self.jug > 0:
+            transferobj = jug5(self.jug)
+            transferobj.full()
+            return
+        return transferobj.full()
+
+
+class jug5():
+    def __init__(self,):
+        self.jug = 0
+
+    def value(self):
+        return self.jug
+
+    def full(self):
+        self.jug = 5
 
     def empty(self):
         self.jug = 0
 
+    def receiver(self, value):
+        if self.jug + value <= 5:
+            self.jug += value
+            return value
+        elif self.jug + value > 5:
+            ley = self.jug
+            self.jug = 5
+            return (ley+value)-5
 
-class jug5():
+    def transfersend(self):
+        if self.jug > 0:
+            tranferobj = jug3()
+            tranferobj.receiver(self.jug)
+            return
+        elif self.jug == 0:
+            print('nothing to transfer...')
+
+
+class playgamejug():
+    def __init__(self):
+        print("The game consis in leave with 4 liters the jug of 5 liters, emptying or filling the jug of 3 or 5 liters\nLET'US BEGIN!\n")
+
+    def play(self):
+        while True:
+            while True:
+                a = int(input(
+                    'Introduce 1 if you want fill the of 3 litter or 2 if you want to fill the jug of 5 litters: '))
+                if a == 1:
+                    j = jug3()
+                    j.full()
+                    b = jug5()
+                    break
+                elif a == 2:
+                    b = jug5()
+                    b.full()
+                    j = jug3()
+                    break
+                else:
+                    print('dont be any jug')
+            if b.value() == 4 or j.value() == 4:
+                print('YOU WIN')
+                break
+            while True:
+                transfer = int(
+                    input('transfer any jug? if is yes tell me which.. 1 or 2 or 3 if you want pass'))
+                if transfer == 1:
+                    j.transfersend()
+                    break
+                elif transfer == 2:
+                    b.transfersend()
+                    break
+                elif transfer == 3:
+                    break
+                else:
+                    print('Donde be any jug')
+            while True:
+                empty = int(
+                    input('What jug you want to empty? 1 or 2? or 3 if you want to pass'))
+                if empty == 1:
+                    j.empty()
+                    print('the jug of 3 litter be empty')
+                    break
+                elif empty == 2:
+                    b.empty()
+                    print('the jug of 5 litter be empty')
+                    break
+                elif empty == 3:
+                    break
+                else:
+                    print('Dont be any jug')
+
+
+a = playgamejug()
+a.play()
