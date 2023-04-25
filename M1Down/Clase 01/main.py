a09e0fd5ede3faddaccc6317509582c66f4e3347
EmilianoEmanuelSosa <emilianoemanuel2850@gmail.com>
2023-04-25 01:24:04 -0300

    rearranging

--- M1Down/Clase 01/main.py
+++ M1Down/Clase 01/main.py
@@ -1,4 +1,6 @@
 import math
+
+
 def Numberbinary(numero):
     '''
     Esta función recibe como parámetro un número entero mayor ó igual a cero y lo devuelve en su
@@ -19,31 +21,27 @@ def Numberbinary(numero):
             return binario
 
 
-
-
-
-
-
-
-
 '''2) Convertir de decimal a binario las fracciones 1/2, 1/3, 1/4, 1/5, 1/6, 1/7,
 1/8, 1/9. Luego analizar los resultados y observar qué particularidad se encuentra
 en los mismos. Se puede usar Python o una calculadora, lo importante es ver si hay
 algo que podemos notar...'''
 
+
 def numberfloat(num):
     final = '0.'
-    while num <2:
-        num*=2
+    while num < 2:
+        num *= 2
         if num < 0:
-            final +='0'
+            final += '0'
         elif num >= 1:
-            final+='1'
-            num-=1
-        if num==0:
+            final += '1'
+            num -= 1
+        if num == 0:
             break
     return float(final)
-lis = [1/2,1/3,1/4,1/5,1/6,1/7,1/8,1/9]
+
+
+lis = [1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8, 1/9]
 for i in lis:
     print(i)
     print(numberfloat(i))
